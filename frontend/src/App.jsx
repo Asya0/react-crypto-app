a11fb38287e48c4dbaaa7d11fc8899a744bb127e
Asya0 <anya.gerasimenko763@mail.ru>
2024-06-21 01:15:28 +0600

    feat: added structure of the service

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-import AppLayout from './components/layout/AppLayout'
-import { CryptoContextProvider } from './context/crypto-context'
+// import AppLayout from './components/layout/AppLayout'
+// import { CryptoContextProvider } from "./context/crypto-context";
+import React from "react";
+import { Layout } from "antd";
+import AppHeader from "./components/layout/AppHeader";
+import AppContent from "./components/layout/AppContent";
+import AppSider from "./components/layout/AppSider";
+
+const siderStyle = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#1677ff",
+};
 
 export default function App() {
   return (
-    <CryptoContextProvider>
-      <AppLayout />
-    </CryptoContextProvider>
-  )
+    <Layout>
+      <AppHeader />
+      <Layout>
+        <AppSider />
+        <AppContent />
+      </Layout>
+    </Layout>
+  );
 }
